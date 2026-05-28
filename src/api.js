713f9b54a1d2308ebea986967259f05556035472
sainamariya10 <sainamariya10@gmail.com>
2026-05-28 03:41:37 -0700

    fix baseURL to include /api

--- src/api.js
+++ src/api.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
 const API = axios.create({
- baseURL: "https://fitness-tracker-with-goals.onrender.com",
-
-   headers: {
+  baseURL: "https://fitness-tracker-with-goals.onrender.com/api",
+  headers: {
     'Content-Type': 'application/json'
   }
 });
